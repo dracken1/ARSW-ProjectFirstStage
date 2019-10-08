016c3293d5d82d8f5f4655e270d8eb015b9af1cb
Pedro <pedrojmayorga@gmail.com>
2019-10-08 18:20:03 -0500

    Room, User and Map Entities Made

--- src/main/java/arsw/bloodymoney/entities/Map.java
+++ src/main/java/arsw/bloodymoney/entities/Map.java
@@ -1,4 +1,84 @@
 package arsw.bloodymoney.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Class Map that defines the
+ * Maps used by a Player
+ * @author Pedro Mayorga - PeNav
+ * @version 1.0
+ * @since 2019-09-23
+ */
 public class Map {
+    
+    private int id;
+    private String name;
+    private String type;
+    private String difficulty;
+    
+    //private List<Obstacle> obstacles;
+    //private List<Coin> coins;
+    //private List<Door> doors;
+    
+    
+    //Constructors
+
+    public Map() {
+    }
+
+    public Map(int id) {
+        this.id = id;
+    }
+
+    public Map(int id, String name, String type, String difficulty) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.difficulty = difficulty;
+    }
+    
+    
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    
+    
+    //To String
+
+    @Override
+    public String toString() {
+        return "Map{" + "id=" + id + ", name=" + name + ", type=" + type + ", difficulty=" + difficulty + '}';
+    }    
+    
 }
--- src/main/java/arsw/bloodymoney/entities/Room.java
+++ src/main/java/arsw/bloodymoney/entities/Room.java
@@ -13,27 +13,123 @@ import java.util.List;
 public class Room implements Serializable{
     
     private int id;
+    private String name;
     private int capacity;
-    private String remainingTime;
-    private int rounds;
+    private int stages;
+    private int time;
+    private String remainingTime;    
     
     private List<Map> maps;
     private List<User> users;
     
-    //TimeinMinutes
-    //long start = System.currentTimeMillis();
-    //long elapsedTimeMillis = System.currentTimeMillis()-start;
-    //float elapsedTimeMin = elapsedTimeMillis/(60*1000F);
-    //float elapsedTimeSec = elapsedTimeMillis/1000F;
+
+    //Constructors
     
-    private int idSala = 0;
-    // el id se deberia crear automaticamente
-
-    public Room(int idSala){
-        this.idSala = idSala;
+    public Room() {
     }
 
-    public int getIdSala() {
-        return idSala;
+    public Room(int id) {
+        this.id = id;
     }
+    
+    public Room(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Room(int id, String name, int capacity, int stages) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.stages = stages;
+    }
+
+    public Room(int id, String name, int capacity, int stages, int time) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.stages = stages;
+        this.time = time;
+    }
+
+    
+    //Getters and Setters
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getStages() {
+        return stages;
+    }
+
+    public void setStages(int stages) {
+        this.stages = stages;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(String remainingTime) {
+        //long start = System.currentTimeMillis();
+        //long elapsedTimeMillis = System.currentTimeMillis()-start
+        //Time in minutes and seconds
+        //float elapsedTimeMin = elapsedTimeMillis/(60*1000F);
+        //float elapsedTimeSec = elapsedTimeMillis/1000F;
+        this.remainingTime = remainingTime;
+    }
+
+    public List<Map> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(List<Map> maps) {
+        this.maps = maps;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    
+    //To String
+    
+    @Override
+    public String toString() {
+        return "Room{" + "id=" + id + ", name=" + name + ", capacity=" + capacity + ", stages=" + stages + ", time=" + time + ", remainingTime=" + remainingTime + ", maps=" + maps + ", users=" + users + '}';
+    }
+ 
 }
