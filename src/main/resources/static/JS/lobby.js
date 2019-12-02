d2434656b992b7240b47c54207de255e1899fd4a
Pedro Mayorga <pedrojmayorga@gmail.com>
2019-12-02 03:40:51 -0500

    Headers and footers

--- src/main/resources/static/JS/lobby.js
+++ src/main/resources/static/JS/lobby.js
@@ -4,6 +4,23 @@ var user;
 var stats;
 var url = '/sessions/' + getCookie("username");
 var urlstats = '/stats/' + getCookie("username");
+
+fetch(url, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log("\n\n\n\n\n\nTEEEEEEEMP\n\n\n\n\n\n");
+        user = data.user;
+        console.log(user);
+        document.getElementById('userUsernameData').innerHTML = "" + user.username;
+        document.getElementById('userNameData').innerHTML = "" + user.name + " " + user.lastname;
+        document.getElementById('userEmailData').innerHTML = "" + user.email;
+    });
+
 fetch(url, {
     method: 'GET',
     headers: {
@@ -41,12 +58,12 @@ fetch(urlstats, {
             '<tbody>';
         console.log(data);
         console.log(data.length);
-        for (var i in data){
-            tableToWrite +='<tr><td class="name">'+ i +'</td>' +
-                            '<td class="name">'+ data[i].date +'</td>' +
-                            '<td class="name">'+ data[i].exp +'</td>' +
-                            '<td class="name">'+ data[i].score +'</td>' +
-                            '<td class="name">'+ data[i].type +'</td></tr>';
+        for (var i in data) {
+            tableToWrite += '<tr><td class="name">' + i + '</td>' +
+                '<td class="name">' + data[i].date + '</td>' +
+                '<td class="name">' + data[i].exp + '</td>' +
+                '<td class="name">' + data[i].score + '</td>' +
+                '<td class="name">' + data[i].type + '</td></tr>';
         }
         tableToWrite += '</tbody></table>';
         console.log(tableToWrite);
@@ -54,41 +71,41 @@ fetch(urlstats, {
         htmlstatstable.innerHTML = tableToWrite;
     });
 
-$('.genericbtn').click(function(){
+$('.genericbtn').click(function () {
     window.location.href = "practice.html";
 });
 
-$('.practicebtn').click(function(){
+$('.practicebtn').click(function () {
     window.location.href = "practice.html";
 });
 
-$('.htitle').click(function(){
+$('.htitle').click(function () {
     console.log(user);
-    if (document.getElementById('extendeduserlogoid').clientHeight > 0){
+    if (document.getElementById('extendeduserlogoid').clientHeight > 0) {
         $("#extendeduserlogoid").animate({
             height: "toggle"
-        },200);
+        }, 200);
     }
 });
-$('.hspace').click(function(){
-    if (document.getElementById('extendeduserlogoid').clientHeight > 0){
+$('.hspace').click(function () {
+    if (document.getElementById('extendeduserlogoid').clientHeight > 0) {
         $("#extendeduserlogoid").animate({
             height: "toggle"
-        },200);
+        }, 200);
     }
 });
-$('.middlebar').click(function(){
-    if (document.getElementById('extendeduserlogoid').clientHeight > 0){
+$('.middlebar').click(function () {
+    if (document.getElementById('extendeduserlogoid').clientHeight > 0) {
         $("#extendeduserlogoid").animate({
             height: "toggle"
-        },200);
+        }, 200);
     }
 });
-$('.footerbar').click(function(){
-    if (document.getElementById('extendeduserlogoid').clientHeight > 0){
+$('.footerbar').click(function () {
+    if (document.getElementById('extendeduserlogoid').clientHeight > 0) {
         $("#extendeduserlogoid").animate({
             height: "toggle"
-        },200);
+        }, 200);
     }
 });
 function mousePressed(obj) {
@@ -110,57 +127,57 @@ function mouseLeave(obj) {
 $("#haccountid").click(function () {
     $("#extendeduserlogoid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
 $("#settoptsid").click(function () {
     $("#settdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
-$("#settexitbtnid").click(function(){
+$("#settexitbtnid").click(function () {
     $("#settdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
 $("#insoptsid").click(function () {
     $("#instructdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
-$("#instructexitbtnid").click(function(){
+$("#instructexitbtnid").click(function () {
     $("#instructdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
 $("#abooptsid").click(function () {
     $("#aboutdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
-$("#aboutexitbtnid").click(function(){
+$("#aboutexitbtnid").click(function () {
     $("#aboutdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
 function getCookie(name) {
-  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
-  var result = regexp.exec(document.cookie);
-  return (result === null) ? null : result[1];
+    var regexp = new RegExp("(?:^" + name + "|;\s*" + name + ")=(.*?)(?:;|$)", "g");
+    var result = regexp.exec(document.cookie);
+    return (result === null) ? null : result[1];
 }
 
-$("#exitbtnid").click(function(){
+$("#exitbtnid").click(function () {
     $("#accountdisplayableid").animate({
         height: "toggle"
-    },200);
+    }, 200);
 });
 
-$("#accoptsid").click(function (){
-   $("#accountdisplayableid").animate({
-       height: "toggle"
-   },200);
+$("#accoptsid").click(function () {
+    $("#accountdisplayableid").animate({
+        height: "toggle"
+    }, 200);
 });
 
-$("#outoptsiconid").click(function(){
+$("#outoptsiconid").click(function () {
     document.cookie = "username=; expires=Thu, 21 Aug 1970 20:00:00 UTC; path=/;"
     document.cookie = "; expires=Thu, 21 Aug 1970 20:00:00 UTC; path=/;"
     console.log(document.cookie);
@@ -171,13 +188,13 @@ var dibujarLobby = function (tabla) {
     //alert(tabla);
     $("#tablaSalas tbody").empty(); // limpiar tabla
 
-    tabla.map(function (salaDescripcion){
+    tabla.map(function (salaDescripcion) {
         var salaId = salaDescripcion.descripcion;
         var cantidadJudadores = salaDescripcion.cantidadJugadores;
-        if(cantidadJudadores==2){
-            $("#tablaSalas tbody").append("<tr><td>Lobby "+ salaId +"</td><td>"+cantidadJudadores+"</td><td> <button style='background-color: darkred; color: white' class=\"genericbtn\" id=\"gnrbtnlft\">GAME FULL</button></td></tr>");
-        }else{
-            $("#tablaSalas tbody").append("<tr><td>Lobby "+ salaId +"</td><td>"+cantidadJudadores+"</td><td> <button onclick='cargaPage().unirseASala("+salaId+")' class=\"genericbtn\" id=\"gnrbtnlft\">JOIN GAME</button></td></tr>");
+        if (cantidadJudadores == 2) {
+            $("#tablaSalas tbody").append("<tr><td>Lobby " + salaId + "</td><td>" + cantidadJudadores + "</td><td> <button style='background-color: darkred; color: white' class=\"genericbtn\" id=\"gnrbtnlft\">GAME FULL</button></td></tr>");
+        } else {
+            $("#tablaSalas tbody").append("<tr><td>Lobby " + salaId + "</td><td>" + cantidadJudadores + "</td><td> <button onclick='cargaPage().unirseASala(" + salaId + ")' class=\"genericbtn\" id=\"gnrbtnlft\">JOIN GAME</button></td></tr>");
         }
     })
 
@@ -186,29 +203,29 @@ var dibujarLobby = function (tabla) {
 var getSalas = function () {
     //alert("entro a get salas");
     var traerSalas = $.get("/Sala/get");
-    traerSalas.then(function(salas) {
+    traerSalas.then(function (salas) {
         //alert("antes de dibujar lobby");
         dibujarLobby(JSON.parse(salas));
-    },function () {
+    }, function () {
         //alert("no sabemos si el alert este func");
     });
     return traerSalas;
 };
 
-(function(){
+(function () {
     getSalas();
     console.info('Connecting to WS...');
     var username = getCookie("username");
-    var usuarioJSON = {username: username};
+    var usuarioJSON = { username: username };
     var socket = new SockJS('/stompendpoint');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        stompClient.send("/app/salas", {},JSON.stringify(usuarioJSON));
-        stompClient.subscribe('/topic/nuevaSala',function (eventbody) {
+        stompClient.send("/app/salas", {}, JSON.stringify(usuarioJSON));
+        stompClient.subscribe('/topic/nuevaSala', function (eventbody) {
             getSalas();
         });
-        stompClient.subscribe('/topic/salas',function (eventbody) {
+        stompClient.subscribe('/topic/salas', function (eventbody) {
             getSalas();
         });
 
@@ -221,18 +238,18 @@ var cargaPage = function () {
 
 
 
-    return{
+    return {
         unirseASala: function (salaId) {
             var username = getCookie("username");
-            var usuarioJSON = {username: username};
-            stompClient.send("/app/unirseASala."+salaId, {},JSON.stringify(usuarioJSON));
-            stompClient.send("/app/salas", {},JSON.stringify(usuarioJSON));
-            window.location.href = "combat.html?id="+salaId;
+            var usuarioJSON = { username: username };
+            stompClient.send("/app/unirseASala." + salaId, {}, JSON.stringify(usuarioJSON));
+            stompClient.send("/app/salas", {}, JSON.stringify(usuarioJSON));
+            window.location.href = "combat.html?id=" + salaId;
         },
         createLobby: function () {
-            stompClient.send("/app/nuevaSala", {},null);
+            stompClient.send("/app/nuevaSala", {}, null);
         },
-        getCookies : getCookie()
+        getCookies: getCookie()
     };
 
 }
