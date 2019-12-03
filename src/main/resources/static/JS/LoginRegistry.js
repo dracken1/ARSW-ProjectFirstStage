81552120a20faf46a62522e8a153253554f0a071
Pedro Mayorga <pedrojmayorga@gmail.com>
2019-12-03 01:29:14 -0500

    login full

--- src/main/resources/static/JS/LoginRegistry.js
+++ src/main/resources/static/JS/LoginRegistry.js
@@ -1,19 +1,21 @@
 
 $("#btntoggle").click(function () {
-    $("#login").animate({height:"toggle",
+    $("#login").animate({
+        height: "toggle",
         'padding-top': "toggle",
         'padding-bottom': "toggle",
         opacity: "toggle"
     });
-    $("#sigin").animate({height: "toggle",
+    $("#sigin").animate({
+        height: "toggle",
         paddingTop: "toggle",
         paddingBottom: "toggle",
         opacity: "toggle"
     });
 });
 
-class User{
-    constructor(name,lastname,username,email,password){
+class User {
+    constructor(name, lastname, username, email, password) {
         this.name = name;
         this.lastname = lastname;
         this.username = username;
@@ -24,28 +26,41 @@ class User{
 
 var usuario = document.cookie;
 var usertodisplay;
-$("#btnlogin").click(function(){
-       var user = document.getElementById('loginuser').value;
-       var password = document.getElementById('loginpass').value;
-       var url = '/session/' + user + '/' + password;
-           fetch(url, {
-               method: 'GET',
-               headers: {
-                   'Content-Type': 'application/json'
-               }
-           })
-           .then(response => response.json())
-           .then(data => {
-               console.log(data);
-               if(data.user.name != null){
-                   usuario = new User(data.user.name, data.user.lastname, data.user.username, data.user.email, data.user.password);
-                   document.cookie = "username=" + data.user.username;
-                   window.location.href = "home.html";
-               } else {
-                   alert("Incorrect username or password");
-               }
-           })
-   });
+$("#btnlogin").click(function () {
+    var user = document.getElementById('loginuser').value;
+    var password = document.getElementById('loginpass').value;
+    var url = '/session/' + user + '/' + password;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.user.name != null) {
+                usuario = new User(data.user.name, data.user.lastname, data.user.username, data.user.email, data.user.password);
+                document.cookie = "username=" + data.user.username;
+                window.location.href = "home.html";
+            } else {
+                alert("Incorrect Username or Password");
+            }
+        })
+});
+
+// Execute a function when the user releases a key on the keyboard
+document.getElementById("login").addEventListener("keypress", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btnlogin").click();
+
+
+    }
+});
 
 /*$("#btnregistry").click(function(){
    var firstname = document.getElementById('rfname').value;
@@ -71,65 +86,65 @@ $("#btnlogin").click(function(){
        })
 });*/
 
-var signinfunction = function(){
+var signinfunction = function () {
     return {
         signin: function () {
             var usernametocheck = document.getElementById('rusername').value;
             var emailtocheck = document.getElementById('remail').value;
             console.log(emailtocheck);
             var url = '/session/' + usernametocheck;
-               fetch(url, {
-                   method: 'GET',
-                   headers: {
-                       'Content-Type': 'application/json'
-                   }
-               })
-               .then(response => response.json())
-               .then(data => {
-                   console.log("duplicated username: " + data);
-                   if(data === true){
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("duplicated username: " + data);
+                    if (data === true) {
                         alert("Username already exists!");
-                   } else{
+                    } else {
 
                         var url2 = '/session/user/' + emailtocheck;
-                           fetch(url2, {
-                               method: 'GET',
-                               headers: {
-                                   'Content-Type': 'application/json'
-                               }
-                           })
-                           .then(response2 => response2.json())
-                           .then(data2 => {
-                               console.log("Used email: " + data2);
-                               if(data2 === true){
+                        fetch(url2, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                            .then(response2 => response2.json())
+                            .then(data2 => {
+                                console.log("Used email: " + data2);
+                                if (data2 === true) {
                                     alert("Email already in use!");
-                               }else{
+                                } else {
                                     var firstname = document.getElementById('rfname').value;
-                                       var lastname = document.getElementById('rlname').value;
-                                       var username = document.getElementById('rusername').value;
-                                       var email = document.getElementById('remail').value;
-                                       var password = document.getElementById('rpassword').value;
-                                       var usertosend = new User(firstname,lastname,username,email,password);
-                                       var url3 = '/session/add';
-                                           fetch(url3, {
-                                               method: 'POST',
-                                               headers: {
-                                                   'Content-Type': 'application/json'
-                                               },
-                                               body:JSON.stringify(usertosend)
-                                           })
-                                           .then(response => {
-                                               if(response.ok){
-                                                    alert("You´ve succesfully created your account!")
-                                                   window.location.href = "home.html";
-                                               } else {
-                                                   alert("Something went wrong!");
-                                               }
-                                           })
-                               }
-                           })
-                   }
-               })
+                                    var lastname = document.getElementById('rlname').value;
+                                    var username = document.getElementById('rusername').value;
+                                    var email = document.getElementById('remail').value;
+                                    var password = document.getElementById('rpassword').value;
+                                    var usertosend = new User(firstname, lastname, username, email, password);
+                                    var url3 = '/session/add';
+                                    fetch(url3, {
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify(usertosend)
+                                    })
+                                        .then(response => {
+                                            if (response.ok) {
+                                                alert("You´ve succesfully created your account!")
+                                                window.location.href = "home.html";
+                                            } else {
+                                                alert("Something went wrong!");
+                                            }
+                                        })
+                                }
+                            })
+                    }
+                })
         }
     }
 }
